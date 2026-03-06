f0d129b787a7e19c119679bdb93d7a886ca947ae
CallMeNezu <mosenkomihail49@gmail.com>
2026-03-06 11:18:39 +0700

    Update task_2.2.py

--- task_2.2.py
+++ task_2.2.py
@@ -6,7 +6,9 @@ dick_usage = psutil.disk_usage("/")
 while True:
     output_1 = f"""Нагрузка CPU: {psutil.cpu_percent(interval=1)}% 
 использовано оперативной памяти: {memory.used/(1024**3):.2f} ГБ 
-процентное использование жесткого диска: {dick_usage.percent:.1f}%"""
+процентное использование жесткого диска: {dick_usage.percent:.1f}%
+"""
     print(output_1)
     print("\n")
     time.sleep(3)
+
